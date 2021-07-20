b371d2d27458167db52ae9b695763a2b9d88de08
kishanhitk <kishansharma1231@gmail.com>
2021-07-20 12:59:45 +0530

    ✨ Add basic cashier calculator

---
+++ util/countCurrency.ts
@@ -0,0 +1,17 @@
+export type Currency = {
+  note: number;
+  count: number;
+};
+
+export function countCurrency(amount: number): Currency[] {
+  var notes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+  var result: Currency[] = [];
+  for (var i = 0; i < notes.length; i++) {
+    var count = Math.floor(amount / notes[i]);
+    if (count > 0) {
+      result.push({ note: notes[i], count: count });
+      amount -= count * notes[i];
+    }
+  }
+  return result;
+}
