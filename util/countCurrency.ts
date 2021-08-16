fe69176673f230181afa32058d8df93ba4e0a32a
Kishan Kumar <41117038+kishanhitk@users.noreply.github.com>
2021-08-16 13:34:46 +0530

    fix: Remove deprecated cash values

--- util/countCurrency.ts
+++ util/countCurrency.ts
@@ -4,7 +4,7 @@ export type Currency = {
 };
 
 export function countCurrency(amount: number): Currency[] {
-  var notes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+  var notes = [2000, 500, 100, 20, 10, 5, 2, 1];
   var result: Currency[] = [];
   for (var i = 0; i < notes.length; i++) {
     var count = Math.floor(amount / notes[i]);
